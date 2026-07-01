76cb01641c786a344d94457a92eb8abd0bdb5c02
Rakib2275 <rakib932676@gmail.com>
2026-07-01 21:56:21 +0600

    Comment section

--- src/modules/comments/comment.routes.ts
+++ src/modules/comments/comment.routes.ts
@@ -1,9 +1,43 @@
 import { Router } from "express";
+import { Role } from "../../../generated/prisma/enums";
+import { auth } from "../../middlewares/auth";
+import { commentController } from "./comment.controller";
 
 const router = Router();
 
-// router.post("/login",authController.loginUser);
-// router.post("/refresh-token",authController.refreshToken);
+router.post( 
+    "/",
+    auth(Role.USER, Role.ADMIN),
+    commentController.createComment
+);
+
+router.get(
+    "/author/:authorId",
+    commentController.getCommentByAuthorId
+);
+
+router.get(
+    "/:postId",
+    commentController.getCommentByPostId
+);
+
+router.patch(
+    "/:commentId",
+    auth(Role.USER, Role.ADMIN),
+    commentController.updateComment
+);
+
+router.delete(
+    "/:commentId",
+    auth(Role.USER, Role.ADMIN),
+    commentController.deleteComment
+);
+
+router.put(
+    "/:commentId/moderate",
+    auth(Role.ADMIN),
+    commentController.moderateComment
+);
 
 
-export const commentRouters = router;
+export const commentRoutes = router;
